13315cc366ebf231e052d4761057a8e1e5a2dd58
19ce074 <19ce074@charusat.edu.in>
2023-05-22 17:57:21 +0530

    Hello vivek

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         <code>This is a React app</code> .
+         <code>Hello Vivek</code> .
         </p>
         <a
           className="App-link"
